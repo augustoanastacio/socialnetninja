ed7f5219a4314de22013a0c4f4a9f6f909b8c01f
Augusto Anastacio <augustoanastacio12@gmail.com>
2018-06-26 04:29:17 -0300

    construtor do panel about view para grupos pronto

--- src/view/About/AboutView.java
+++ src/view/About/AboutView.java
@@ -37,6 +37,16 @@ public class AboutView extends javax.swing.JPanel {
 
     public AboutView(Group group) {
         initComponents();
+        //esperando metodos do grupo;
+        lblname.setText(group.getName());
+        lblDob.setText(group.getDateCreatedString());
+        txtAreaBio.setText(group.getBio());
+        ImageIcon image = group.getProfilePicture();
+        lblfoto.setIcon(new ImageIcon(image.getImage().getScaledInstance(lblfoto.getWidth(), lblfoto.getHeight(), Image.SCALE_SMOOTH)));
+        lbladdresslbl.setVisible(false);
+        lbldatalbl.setText("Data de Criação:");
+        lblfonelbl.setVisible(false);
+        lblBiolbl.setText("Descrição:");
         
     }
 
@@ -53,26 +63,26 @@ public class AboutView extends javax.swing.JPanel {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         lblname = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        lbldatalbl = new javax.swing.JLabel();
         lblDob = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
+        lbladdresslbl = new javax.swing.JLabel();
         lblAddress = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
+        lblfonelbl = new javax.swing.JLabel();
         lblPhone = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
+        lblBiolbl = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
         txtAreaBio = new javax.swing.JTextArea();
         lblfoto = new javax.swing.JLabel();
 
         jLabel1.setText("Nome:");
 
-        jLabel2.setText("Data de Nascimento:");
+        lbldatalbl.setText("Data de Nascimento:");
 
-        jLabel3.setText("Endereço:");
+        lbladdresslbl.setText("Endereço:");
 
-        jLabel4.setText("Telefone:");
+        lblfonelbl.setText("Telefone:");
 
-        jLabel5.setText("Biografia:");
+        lblBiolbl.setText("Biografia:");
 
         txtAreaBio.setEditable(false);
         txtAreaBio.setColumns(20);
@@ -90,16 +100,16 @@ public class AboutView extends javax.swing.JPanel {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(lblPhone, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel5)
+                            .addComponent(lblBiolbl)
                             .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 369, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel2)
+                            .addComponent(lbldatalbl)
                             .addComponent(lblAddress, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel4)
+                            .addComponent(lblfonelbl)
                             .addComponent(lblDob, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel3)
+                            .addComponent(lbladdresslbl)
                             .addComponent(jLabel1)
                             .addComponent(lblname, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
@@ -116,22 +126,22 @@ public class AboutView extends javax.swing.JPanel {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(lblname, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel2)
+                        .addComponent(lbldatalbl)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(lblDob, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel3)
+                        .addComponent(lbladdresslbl)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(lblAddress, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel4))
+                        .addComponent(lblfonelbl))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(20, 20, 20)
                         .addComponent(lblfoto, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(lblPhone, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel5)
+                .addComponent(lblBiolbl)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(66, Short.MAX_VALUE))
@@ -154,16 +164,16 @@ public class AboutView extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel lblAddress;
+    private javax.swing.JLabel lblBiolbl;
     private javax.swing.JLabel lblDob;
     private javax.swing.JLabel lblPhone;
+    private javax.swing.JLabel lbladdresslbl;
+    private javax.swing.JLabel lbldatalbl;
+    private javax.swing.JLabel lblfonelbl;
     private javax.swing.JLabel lblfoto;
     private javax.swing.JLabel lblname;
     private javax.swing.JTextArea txtAreaBio;
